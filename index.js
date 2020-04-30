084a0f5dfc8081f8a94293199b88a4e5d5846166
iamsainikhil <sainikhil.bheema@gmail.com>
2020-04-30 03:24:39 -0400

    use cors and add whitelisted domains

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const axios = require('axios')
+const cors = require('cors')
 const port = process.env.PORT || 3001
 
 // Configure app to use bodyParser to parse json data
 const app = express()
-const server = require('http').createServer(app)
 
+// whitelisted domains
+const WHITELISTED_DOMAINS = process.env.WHITELISTED_DOMAINS
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true)
+      if (WHITELISTED_DOMAINS.split(',').indexOf(origin) === -1) {
+        var msg = `The CORS policy for this site does not allow access from the specified ${origin}.`
+        return callback(new Error(msg), false)
+      }
+      return callback(null, true)
+    },
+  })
+)
+
+const server = require('http').createServer(app)
 require('dotenv').config()
 
 // custom HTTP headers for authenticating requests sent to Algolia places server
@@ -13,26 +32,19 @@ const HEADERS = {
   'X-Algolia-Application-Id': process.env.REACT_APP_ALGOLIA_PLACES_APP_ID || '',
   'X-Algolia-API-Key': process.env.REACT_APP_ALGOLIA_PLACES_API_KEY || '',
 }
-
 const DARKSKY_API_KEY = process.env.REACT_APP_DARKSKY_API_KEY
 
-// Test server is working (GET http://localhost:3001/api)
+// Test server is working (GET http://localhost:3001/)
 app.get('/', function (req, res) {
   const host = req.get('host')
-  const origin = req.get('origin')
-  console.log(host, origin)
   res.json({
     message: 'Welcome to the Weather React application API!',
     host: host,
-    origin: origin,
   })
 })
 
 // Fetch weather forecast based on latlong
 app.get('/forecast/coords/:latlong', (req, res) => {
-  const host = req.get('host')
-  const origin = req.get('origin')
-  console.log(host, origin)
   const {latlong} = req.params
   const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${latlong}?extend=hourly&exclude=minutely,flags`
   axios
@@ -52,9 +64,6 @@ app.get('/forecast/coords/:latlong', (req, res) => {
 
 // Fetch address based on latlong
 app.get('/address/coords/:latlong', (req, res) => {
-  const host = req.get('host')
-  const origin = req.get('origin')
-  console.log(host, origin)
   const {latlong} = req.params
   const url = `https://places-dsn.algolia.net/1/places/reverse?aroundLatLng=${latlong},&hitsPerPage=1&language=en`
   axios
@@ -74,7 +83,6 @@ app.get('/address/coords/:latlong', (req, res) => {
 
 // Fetch address list based on query
 app.get('/places/query/:city/:latlong', (req, res) => {
-  console.log(req.params)
   const {city, latlong} = req.params
   axios
     .request({
