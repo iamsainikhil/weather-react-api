03a54748de8762f5504e152e821ae6851e7fca3f
iamsainikhil <sainikhil.bheema@gmail.com>
2020-04-30 03:40:54 -0400

    add customer CORS headers

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const port = process.env.PORT || 3001
 const app = express()
 
 // whitelisted domains
-const WHITELISTED_DOMAINS = process.env.WHITELISTED_DOMAINS
+// const WHITELISTED_DOMAINS = process.env.WHITELISTED_DOMAINS
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true)
-      if (WHITELISTED_DOMAINS.split(',').indexOf(origin) === -1) {
-        var msg = `The CORS policy for this site does not allow access from the specified ${origin}.`
-        return callback(new Error(msg), false)
-      }
-      return callback(null, true)
-    },
-  })
-)
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true)
+//       if (WHITELISTED_DOMAINS.split(',').indexOf(origin) === -1) {
+//         var msg = `The CORS policy for this site does not allow access from the specified ${origin}.`
+//         return callback(new Error(msg), false)
+//       }
+//       return callback(null, true)
+//     },
+//   })
+// )
 
 const server = require('http').createServer(app)
 require('dotenv').config()
